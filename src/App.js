8082c18a58fded167c79c4500a0e044c3e7de140
andriano <andrianow817@gmail.com>
2022-05-01 23:43:30 +0700

    connect to API and make simple preview card

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { CardLayout } from "./components/MainComponent";
 function App() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    document.body.style.backgroundColor = "gray";
+    const fetchApi = async () => {
+      await axios("https://quran-api-id.vercel.app/surahs").then((response) =>
+        setData(response.data)
+      );
+    };
+    fetchApi();
+    console.log(data);
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="max-w-screen-2xl mx-auto">
+      <h1 className="text-center flex justify-center content-center">
+        Quran App
+      </h1>
+      <div className=" mt-20 py-8  flex flex-row flex-wrap justify-center">
+        {data.map((data) => (
+          <CardLayout key={data.number}>
+            <div className="  flex flex-row flex-wrap justify-between">
+              <h2>{data.number}</h2>
+              <h2>{data.name}</h2>
+            </div>{" "}
+            <div className=" flex flex-row flex-wrap justify-between border-t-4">
+              <h2>Jumlah Ayat : </h2>
+              <h2>{data.numberOfAyahs}</h2>
+            </div>
+          </CardLayout>
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/components/MainComponent.js
@@ -0,0 +1,7 @@
+export function CardLayout({ children }) {
+  return (
+    <div className="flex justify-center m-10 flex-col w-60 p-3 bg-white">
+      {children}
+    </div>
+  );
+}
